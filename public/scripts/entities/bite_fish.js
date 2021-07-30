ddd0afaa1e3517fa4eb776e35503ccd4c79181ad
Vincent McKen <mcken.vince@gmail.com>
2021-07-30 23:40:16 +0000

    switched window.fishtank to this.fishtank

--- public/scripts/entities/bite_fish.js
+++ public/scripts/entities/bite_fish.js
@@ -9,10 +9,9 @@ class BiteFish extends GoFish {
   updateOneTick() {
     // This method gets called every PHYSICS_TICK_SIZE_S which is currently every 0.010 seconds
     // This is where you update the position of the sprite.
-    for (let fish of window.fishtank.getProximateDenizens(this.position, 20)) {
-
-      console.log("fish: ", fish);
-      console.log("fish.id: ", fish.id);
+    for (let fish of this.fishtank.getProximateDenizens(this.position, 20)) {
+      // console.log("fish: ", fish);
+      // console.log("fish.id: ", fish.id);
       // fish.kill(5); // kills BiteFish as soon as it spawns
       if (fish instanceof SwitchFish) {
         // console.log("fish: ", fish);
